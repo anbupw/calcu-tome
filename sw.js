1e051db5d1e25bbfe836cf918e40328684f4599a
Sweety <zoelanfikar@yahoo.co.id>
2026-06-13 02:32:44 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'calcu-tome-cache-v1';
+
+// Daftarkan file lokal yang ingin bisa diakses secara offline
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './img/icon-192.png',
+  './img/icon-512.png'
+];
+
+// Tahap Install: Menyimpan file ke dalam Cache browser
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Tahap Aktivasi: Membersihkan cache lama jika ada update
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Tahap Fetch: Mengambil data dari cache jika offline, atau dari internet jika online
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
